cdfdf74019a3154a373a909bce434a6f5f92ab13
Eduardo Nunez <edu.a.n.van.eyl@gmail.com>
2023-01-10 17:11:26 +0800

    - added basic world map

--- scripts.js
+++ scripts.js
@@ -8,12 +8,34 @@ d3.csv("clean_data.csv").then( function(data) {
   const gdp_list = new Array();
   const country_list = new Array();
   const bigmac_price_list = new Array();
+  const country_names_list = new Array();
 
   for (let i=0; i<midyear_data.length; i++){
     gdp_list.push(midyear_data[i]["GDP_dollar"])
     country_list.push(midyear_data[i]["iso_a3"])
     bigmac_price_list.push(midyear_data[i]["dollar_price"])
+    country_names_list.push(midyear_data[i]["name"])
   }
+
+  var data = [{
+    type: "choropleth", 
+    locationmode: "country names", 
+    locations: country_names_list,
+    z: bigmac_price_list,
+    text: country_names_list,
+    autocolorscale: true
+  }];
+
+  var layout = {
+    title: "BigMac Price in USD",
+    geo: {
+      projection: {
+        type: "robinson"
+      }
+    }
+  };
+
+  Plotly.newPlot("world_map", data, layout, {showLink: false});
 
   // Scatter Plot
   var trace1 = {
@@ -38,10 +60,10 @@ d3.csv("clean_data.csv").then( function(data) {
       title: "2022 GDP for Countries [USD]"
     },
     yaxis: {
-      title: "Big Mac Price [USD]"
+      title: "BigMac Price [USD]"
     }
   };
   
-  Plotly.newPlot('my_dataviz', data, layout);
+  Plotly.newPlot('scatter_plot', data, layout);
   
 })
