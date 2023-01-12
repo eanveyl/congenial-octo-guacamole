94e6970bf95643967be72a7001d160ce7fc5c6c3
Dimitrios Peppas <dimpeppas@hotmail.gr>
2023-01-12 18:19:49 +0800

    dynamic data

--- scripts.js
+++ scripts.js
@@ -28,61 +28,36 @@ function linearRegression(inputArray, xLabel, yLabel) {
 
 var dataset;
 const dropdown = document.getElementById("dropdown");
+const gdp_list = new Array();
+const country_list = new Array();
+const bigmac_price_list = new Array();
+const bigmac_price_local = new Array();
+const dollar_ex = new Array();
+const currency_code = new Array();
+const country_names_list = new Array();
 var selected_country;
+var selected_country_index;
+var bigmac_price_list_normalized;
 
 d3.csv("clean_data_euromod.csv", (d) => {
   dataset = d;
   start();
 });
 
-async function start() {
-
-  midyear_data = dataset.slice(57, 129);  // TODO this is not the most flexible way to fetch the data
-
-  const gdp_list = new Array();
-  const country_list = new Array();
-  const bigmac_price_list = new Array();
-  const country_names_list = new Array();
-
-  for (let i = 0; i < midyear_data.length; i++) {
-    gdp_list.push(parseFloat(midyear_data[i]["GDP_dollar"]))
-    country_list.push(midyear_data[i]["iso_a3"])
-    bigmac_price_list.push(parseFloat(midyear_data[i]["dollar_price"]))
-    country_names_list.push(midyear_data[i]["name"])
-  }
-
-  country_names_list.forEach((country) => {
-    var opt = document.createElement('option');
-    opt.value = country;
-    opt.innerHTML = country;
-    dropdown.appendChild(opt);
-  });
-
-  selected_country = country_names_list[0];
-  const selected_country_name_html = document.getElementById("selected_country_name");
-  const selected_country_price_html = document.getElementById("selected_country_price");
-
-  selected_country_name_html.innerHTML = selected_country;
-  selected_country_price_html.innerHTML = bigmac_price_list[country_names_list.indexOf(selected_country)];
-
-  dropdown.addEventListener("change", () => {
-    selected_country = dropdown.value;
-  }
-  );
-
+async function draw() {
   // World Map
   // Inspiration from https://plotly.com/javascript/choropleth-maps/
   var data = [{
     type: "choropleth",
     locationmode: "country names",
     locations: country_names_list,
-    z: bigmac_price_list,
-    text: bigmac_price_list,
+    z: bigmac_price_list_normalized,
+    text: bigmac_price_list_normalized,
     autocolorscale: true
   }];
 
   var layout = {
-    title: "BigMac Price in USD",
+    title: "BigMac Price in " + currency_code[selected_country_index],
     geo: {
       projection: {
         type: "robinson"
@@ -144,4 +119,50 @@ async function start() {
   };
 
   Plotly.newPlot('scatter_plot', data, layout);
+}
+
+async function start() {
+
+  midyear_data = dataset.slice(57, 129);
+
+  for (let i = 0; i < midyear_data.length; i++) {
+    gdp_list.push(parseFloat(midyear_data[i]["GDP_dollar"]));
+    country_list.push(midyear_data[i]["iso_a3"]);
+    bigmac_price_list.push(parseFloat(midyear_data[i]["dollar_price"]));
+    bigmac_price_local.push(parseFloat(midyear_data[i]["local_price"]));
+    currency_code.push(midyear_data[i]["currency_code"]);
+    dollar_ex.push(parseFloat(midyear_data[i]["dollar_ex"]));
+    country_names_list.push(midyear_data[i]["name"]);
+  }
+
+  country_names_list.forEach((country) => {
+    var opt = document.createElement('option');
+    opt.value = country;
+    opt.innerHTML = country;
+    dropdown.appendChild(opt);
+  });
+
+  selected_country = country_names_list[0];
+
+  // TODO simplify this
+  const selected_country_name_html = document.getElementById("selected_country_name");
+  const selected_country_price_html = document.getElementById("selected_country_price");
+  selected_country_index = country_names_list.indexOf(selected_country);
+  selected_country_name_html.innerHTML = selected_country;
+  selected_country_price_html.innerHTML = bigmac_price_local[selected_country_index] + " " + currency_code[selected_country_index];
+  bigmac_price_list_normalized = bigmac_price_list.map((price) => price * dollar_ex[selected_country_index]);
+
+  dropdown.addEventListener("change", () => {
+    selected_country = dropdown.value;
+    const selected_country_name_html = document.getElementById("selected_country_name");
+    const selected_country_price_html = document.getElementById("selected_country_price");
+    selected_country_index = country_names_list.indexOf(selected_country);
+    selected_country_name_html.innerHTML = selected_country;
+    selected_country_price_html.innerHTML = bigmac_price_local[selected_country_index] + " " + currency_code[selected_country_index];
+    bigmac_price_list_normalized = bigmac_price_list.map((price) => price * dollar_ex[selected_country_index]);
+    draw();
+  }
+  );
+
+  draw();
 }
